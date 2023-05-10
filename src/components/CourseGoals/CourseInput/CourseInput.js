fc6c06ec3c6af8bb238ba18257ea1b603e4892c5
Raj Dhinge <rajkumar.dhinge.mca15@gmail.com>
2023-05-11 03:33:34 +0530

    :art: Tried styled components and CSS modules component will be prefering CSS module feature

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
-
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+// 	& label {
+// 		font-weight: bold;
+// 		display: block;
+// 		margin-bottom: 0.5rem;
+// 	}
+
+// 	& input {
+// 		display: block;
+// 		width: 100%;
+// 		border: 1px solid #ccc;
+// 		font: inherit;
+// 		line-height: 1.5rem;
+// 		padding: 0 0.25rem;
+// 	}
+
+// 	& input:focus {
+// 		outline: none;
+// 		background: #fad0ec;
+// 		border-color: #8b005d;
+// 	}
+
+// 	&.invalid input{
+// 		background-color: red;
+// 		background: #ffd7d7;
+// 	}
+
+// 	&.invalid label{
+// 		color: red;
+// 	}
+// `;
 
 const CourseInput = props => {
 	const [enteredValue, setEnteredValue] = useState('');
@@ -18,14 +52,15 @@ const CourseInput = props => {
 			setIsValid(false);
 		} else {
 			props.onAddGoal(enteredValue);
+			setEnteredValue('');
 		}
 	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+			<div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
 				<label>Course Goal</label>
-				<input type="text" onChange={goalInputChangeHandler} />
+				<input value={enteredValue} type="text" onChange={goalInputChangeHandler} />
 			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
